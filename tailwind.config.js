47d2dea12e1c07863cc55e5cd7623f382c8e5660
Evgastap <Evgastap@gmail.com>
2021-07-26 20:04:07 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const colors = require('tailwindcss/colors')
+module.exports = {
+   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+      colors: {
+    },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [
+   ],
+  }
