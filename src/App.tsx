b4803f1a1395239d4ba777a88af8af83adff11f8
Evgastap <Evgastap@gmail.com>
2021-07-28 18:56:20 +0200

    random words api proxied through netlify function

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
 function App() {
-
   // state for the words you need to type
   const [words, setWords] = useState({
     prevString: "", // previously typed out letters
@@ -71,11 +70,12 @@ function App() {
   // useEffect to fetch random words on reload and set state
   useEffect(() => {
     fetch(
-      "https://popular-words-api.herokuapp.com/api/words/randomlist?size=50&minrank=1&maxrank=500"
+      "/.netlify/functions/random-words", 
     )
       .then((res) => res.json())
       .then((data) => {
-        data = data.join(" ");
+        // console.log(data.words);
+        data = data.words.join(" ");
         setWords({
           prevString: "",
           currentString: data.substring(0, 1),
@@ -100,9 +100,14 @@ function App() {
           <motion.div
             layout
             transition={{ type: "tween", duration: 0.075 }}
-            className="text-green-300 inline absolute text-xl -mx-1 -my-1 animate-pulse"
+            className="text-green-300 inline absolute text-xl -mx-1 -my-1"
           >
-            |
+            <motion.div
+              animate={{ opacity: [0, 1] }}
+              transition={{ duration: 0.001 }}
+            >
+              |
+            </motion.div>
           </motion.div>
           <div
             className={`${inputWrong ? "text-red-400" : "text-white"} inline`}
