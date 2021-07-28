f3ed6146d8e1b17eeaebe375d050fe2f227175d2
Evgastap <Evgastap@gmail.com>
2021-07-28 12:47:15 +0200

    code formatting, random api, fixed 1 length `inputWrong` bug

--- src/App.tsx
+++ src/App.tsx
@@ -10,31 +10,46 @@ function App() {
 
   const [inputWrong, setInputWrong] = useState("");
 
+  // todo: fix bug where if currentString is 1 in length, and you type the correct string, it will work
   const handleKeyPress = (e: React.KeyboardEvent) => {
     const key = e.key;
-    if (key === words.currentString) { // input is correct
+    if (key === words.currentString && !inputWrong) {
+      // input is correct
       setInputWrong("");
       setWords({
         prevString: words.prevString + words.currentString,
         currentString: words.nextString.substring(0, 1),
         nextString: words.nextString.substr(1),
       });
-    } else if (key === "Backspace") { // if backspace pressed
-      if (inputWrong) { // if there's already a string of wrong pressed keys
+    } else if (key === "Backspace") {
+      // if backspace pressed
+      if (inputWrong) {
+        // if there's already a string of wrong pressed keys
         setInputWrong(inputWrong.substr(0, inputWrong.length - 1));
         setWords({
           prevString: words.prevString,
-          currentString: words.currentString.substring(0, Math.max(words.currentString.length - 1, 1)),
-          nextString: inputWrong.length === 1 ? words.nextString : words.currentString.substr(-1) + words.nextString,
+          currentString: words.currentString.substring(
+            0,
+            Math.max(words.currentString.length - 1, 1)
+          ),
+          nextString:
+            inputWrong.length === 1
+              ? words.nextString
+              : words.currentString.substr(-1) + words.nextString,
         });
-      } else { // if you're just trying to erase a correct key
+      } else {
+        // if you're just trying to erase a correct key
         setWords({
-          prevString: words.prevString.substring( 0, words.prevString.length - 1),
+          prevString: words.prevString.substring(
+            0,
+            words.prevString.length - 1
+          ),
           currentString: words.prevString.substr(-1),
           nextString: words.currentString + words.nextString,
         });
       }
-    } else if (key.match("^[a-zA-Z]$")) { // if the key pressed is incorrect
+    } else if (key.match("^[a-zA-Z]$")) {
+      // if the key pressed is incorrect
       const firstMistake = inputWrong.length === 0;
       setWords({
         prevString: words.prevString,
@@ -50,17 +65,18 @@ function App() {
   };
 
   useEffect(() => {
-    setWords({
-      prevString: "that they ",
-      currentString: "e",
-      nextString:
-        "arly before change long around group you increase eye line against same give turn large where right late more too line place of what school show long or point around plan turn before from people interest course own he write problem too begin change to early give more",
-    });
-    // fetch(
-    //   "https://popular-words-api.herokuapp.com/api/words/list?minrank=1&maxrank=1000"
-    // )
-    //   .then((res) => res.json())
-    //   .then((data) => console.log(data));
+    fetch(
+      "https://popular-words-api.herokuapp.com/api/words/randomlist?size=50&minrank=1&maxrank=500"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        data = data.join(" ");
+        setWords({
+          prevString: "",
+          currentString: data.substring(0, 1),
+          nextString: data.substring(1),
+        });
+      });
   }, []);
 
   return (
@@ -69,20 +85,28 @@ function App() {
       tabIndex={0}
       onKeyDown={(e) => handleKeyPress(e)}
     >
-      <div className="w-3/4 max-w-4xl text-justify bg-gray-800 rounded-lg p-5">
-        <div className="text-blue-400 inline">{words.prevString}</div>
+      {(words.nextString || words.prevString) && (
         <motion.div
-          layout
-          transition={{ type: "tween", duration: 0.075 }}
-          className="text-green-300 inline absolute text-xl -mx-1 -my-1 animate-pulse"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          className="w-3/4 max-w-4xl text-justify bg-gray-800 rounded-lg p-5"
         >
-          |
+          <div className="text-blue-400 inline">{words.prevString}</div>
+          <motion.div
+            layout
+            transition={{ type: "tween", duration: 0.075 }}
+            className="text-green-300 inline absolute text-xl -mx-1 -my-1 animate-pulse"
+          >
+            |
+          </motion.div>
+          <div
+            className={`${inputWrong ? "text-red-400" : "text-white"} inline`}
+          >
+            {/*inputWrong ? inputWrong : */ words.currentString}
+          </div>
+          <div className="text-white inline">{words.nextString}</div>
         </motion.div>
-        <div className={`${inputWrong ? "text-red-400" : "text-white"} inline`}>
-          {/*inputWrong ? inputWrong : */ words.currentString}
-        </div>
-        <div className="text-white inline">{words.nextString}</div>
-      </div>
+      )}
     </div>
   );
 }
