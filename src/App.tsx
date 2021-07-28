7b8d11cd71c0d04caafb9196222a6d29d0cad6c5
Evgastap <Evgastap@gmail.com>
2021-07-28 11:56:37 +0200

    MVP with erasing text & wrong text

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,59 @@
 import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 function App() {
   const [words, setWords] = useState({
-    prevWords: "",
-    currentLetter: "",
-    nextWords: "",
+    prevString: "",
+    currentString: "",
+    nextString: "",
   });
 
-  const handleKeyPress = (key: string) => {
-    if (key === words.currentLetter) {
+  const [inputWrong, setInputWrong] = useState("");
+
+  const handleKeyPress = (e: React.KeyboardEvent) => {
+    const key = e.key;
+    if (key === words.currentString) { // input is correct
+      setInputWrong("");
       setWords({
-        prevWords: words.prevWords + words.currentLetter,
-        currentLetter: words.nextWords.substring(0, 1),
-        nextWords: words.nextWords.substr(1),
+        prevString: words.prevString + words.currentString,
+        currentString: words.nextString.substring(0, 1),
+        nextString: words.nextString.substr(1),
       });
+    } else if (key === "Backspace") { // if backspace pressed
+      if (inputWrong) { // if there's already a string of wrong pressed keys
+        setInputWrong(inputWrong.substr(0, inputWrong.length - 1));
+        setWords({
+          prevString: words.prevString,
+          currentString: words.currentString.substring(0, Math.max(words.currentString.length - 1, 1)),
+          nextString: inputWrong.length === 1 ? words.nextString : words.currentString.substr(-1) + words.nextString,
+        });
+      } else { // if you're just trying to erase a correct key
+        setWords({
+          prevString: words.prevString.substring( 0, words.prevString.length - 1),
+          currentString: words.prevString.substr(-1),
+          nextString: words.currentString + words.nextString,
+        });
+      }
+    } else if (key.match("^[a-zA-Z]$")) { // if the key pressed is incorrect
+      const firstMistake = inputWrong.length === 0;
+      setWords({
+        prevString: words.prevString,
+        currentString: firstMistake
+          ? words.currentString
+          : words.currentString + words.nextString.substring(0, 1),
+        nextString: firstMistake
+          ? words.nextString
+          : words.nextString.substr(1),
+      });
+      setInputWrong(inputWrong + key);
     }
   };
 
   useEffect(() => {
     setWords({
-      prevWords: "that they ",
-      currentLetter: "e",
-      nextWords:
+      prevString: "that they ",
+      currentString: "e",
+      nextString:
         "arly before change long around group you increase eye line against same give turn large where right late more too line place of what school show long or point around plan turn before from people interest course own he write problem too begin change to early give more",
     });
     // fetch(
@@ -33,14 +65,23 @@ function App() {
 
   return (
     <div
-      className="w-screen h-screen bg-gray-600 flex items-center justify-center"
+      className="w-screen h-screen bg-gray-700 flex items-center justify-center"
       tabIndex={0}
-      onKeyDown={(e) => handleKeyPress(e.key)}
+      onKeyDown={(e) => handleKeyPress(e)}
     >
-      <div className="w-3/4 bg-gray-600">
-        <div className="text-pink-400 inline">{words.prevWords}</div>
-        <div className="text-white inline">{words.currentLetter}</div>
-        <div className="text-white inline">{words.nextWords}</div>
+      <div className="w-3/4 max-w-4xl text-justify bg-gray-800 rounded-lg p-5">
+        <div className="text-blue-400 inline">{words.prevString}</div>
+        <motion.div
+          layout
+          transition={{ type: "tween", duration: 0.075 }}
+          className="text-green-300 inline absolute text-xl -mx-1 -my-1 animate-pulse"
+        >
+          |
+        </motion.div>
+        <div className={`${inputWrong ? "text-red-400" : "text-white"} inline`}>
+          {/*inputWrong ? inputWrong : */ words.currentString}
+        </div>
+        <div className="text-white inline">{words.nextString}</div>
       </div>
     </div>
   );
