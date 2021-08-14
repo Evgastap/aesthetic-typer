d80fc73e1924192eaa705f880aa02424152c2f30
Evgastap <Evgastap@gmail.com>
2021-08-14 10:27:52 +0200

    removing react imports

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { motion } from "framer-motion";
 import { Line, defaults } from "react-chartjs-2";
 
--- src/atoms/CorrectWords.tsx
+++ src/atoms/CorrectWords.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const CorrectWords = ({prevString}: { prevString: string } ) => {
   return (
     /* div for correctly typed text */
